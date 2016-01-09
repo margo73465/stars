aeefa3bf8c77f664826553355225853a473b03d1
Margo <margo.smith@venmo.com>
2016-01-09 13:57:54 -0500

    continuously generate, and refactor to make coordinate conversions explicit

--- margo-party.js
+++ margo-party.js
@@ -2,48 +2,64 @@ var height = window.innerHeight;
 var width = window.innerWidth;
 var centerX = width / 2;
 var centerY = height / 2;
-var numStars = 100;
 
 var draw = SVG('drawing').size(width, height);
 
-function convertToPolar(x, y) {
-  r = Math.sqrt(x * x + y * y);
-  theta = Math.atan2(y, x);
-  return [r, theta];
+function svgToCartesian(point) {
+  y = -(point.y - centerY);
+  x = point.x - centerX;
+  return { x: x, y: y };
 }
 
-function convertToCartesian(r, theta) {
-  x = r * Math.cos(theta);
-  y = r * Math.sin(theta);
-  return [x, y]
+function cartesianToSvg(point) {
+  x = point.x + centerX;
+  y = -point.y + centerY;
+  return { x: x, y: y };
 }
 
-function getDestination(x, y) {
-  var destinationX, destinationY;
-
-  y = -(y - centerY);
-  x = x - centerX;
-
-  polar = convertToPolar(x, y);
-  destinationPolar = [polar[0] + 600, polar[1]];
-  destination = convertToCartesian(destinationPolar[0], destinationPolar[1]);
-
-  destinationX = destination[0] + centerX;
-  destinationY = -destination[1] + centerY;
-
-  return [destinationX, destinationY];
+function cartesianToPolar(point) {
+  r = Math.sqrt(point.x * point.x + point.y * point.y);
+  theta = Math.atan2(point.y, point.x);
+  return { r: r, theta: theta };
 }
 
-function generateStars() {
-  for ( var i = 0; i < numStars; i++ ) {
-    var x = Math.random() * width;
-    var y = Math.random() * height;
-
-    var destination = getDestination(x, y);
-
-    var star = draw.rect(2, 2).cx(x).cy(y).attr({ fill: 'white' });
-    star.animate(2000, '-').move(destination[0], destination[1]).loop();
-  }
+function polarToCartesian(polarPoint) {
+  x = polarPoint.r * Math.cos(polarPoint.theta);
+  y = polarPoint.r * Math.sin(polarPoint.theta);
+  return { x: x, y: y };
 }
 
-generateStars();
+function getDestination(point) {
+  cartesianPoint = svgToCartesian(point);
+  polarPoint = cartesianToPolar(cartesianPoint);
+
+  destinationPolar = {
+    r: polarPoint.r + 600,
+    theta: polarPoint.theta
+  };
+
+  destinationCartesian = polarToCartesian(destinationPolar);
+  destination = cartesianToSvg(destinationCartesian);
+  return destination;
+}
+
+function generateRandomPoint() {
+  var x = Math.random() * width;
+  var y = Math.random() * height;
+
+  return { x: x, y: y};
+}
+
+function createStar() {
+  var star = generateRandomPoint();
+  var destination = getDestination(star);
+
+  var star = draw.rect(2, 2).cx(star.x).cy(star.y).attr({ fill: 'white' });
+  star.animate(2000, '-').move(destination.x, destination.y);
+}
+
+function initialize() {
+  window.setInterval(createStar, 10);
+}
+
+initialize();
